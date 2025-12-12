501f7a85b0e9dda3b6d1008eee45e60634d2a44c
Envious Jelly <ema.farnik@gmail.com>
2025-12-12 11:20:30 +0100

    added 1st person movement

--- main.js
+++ main.js
@@ -3,10 +3,16 @@ import { UpdateSystem } from 'engine/systems/UpdateSystem.js';
 import { GLTFLoader } from 'engine/loaders/GLTFLoader.js';
 import { UnlitRenderer } from 'engine/renderers/UnlitRenderer.js';
 
+import { FirstPersonController } from 'engine/controllers/FirstPersonController.js';
+
 import {
     Camera,
+    Entity,
+    Material,
     Model,
-    Entity, // prej imenovan Node
+    Primitive,
+    Sampler,
+    Texture,
     Transform,
 } from 'engine/core/core.js';
 
@@ -21,8 +27,15 @@ await gltfLoader.load(new URL('./game/models/map/map.gltf', import.meta.url)); /
 const scene = gltfLoader.loadScene(gltfLoader.defaultScene);
 const camera = scene.find(node => node.getComponentOfType(Camera));
 
+camera.addComponent(new FirstPersonController(camera, canvas));
 
-function update(time, dt) {}
+function update(time, dt) {
+    for (const entity of scene) {
+        for (const component of entity.components) {
+            component.update?.(time, dt);
+        }
+    }
+}
 
 function render() {
     renderer.render(scene, camera);
@@ -34,3 +47,5 @@ function resize({ displaySize: { width, height }}) {
 
 new ResizeSystem({ canvas, resize }).start();
 new UpdateSystem({ update, render }).start();
+
+const controller = camera.getComponentOfType(FirstPersonController);
